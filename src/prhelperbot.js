7f86df04ed6199debde2a244f0cd744fefad6a60
Mike Dixon <qualitydixon@gmail.com>
2017-03-25 15:41:12 -0600

    Lean in to prettier. Comment out start up message.

--- src/prhelperbot.js
+++ src/prhelperbot.js
@@ -1,91 +1,110 @@
-import Bot from 'slackbots'
-import fs from 'fs'
-import { CronJob } from 'cron'
-import { getSlackName, getMessageText, getReviewers } from './helpers'
+import Bot from "slackbots";
+import fs from "fs";
+import { CronJob } from "cron";
+import { getSlackName, getMessageText, getReviewers } from "./helpers";
 
-if (fs.existsSync('./.env')) require('dotenv').config({path: './.env'})
+if (fs.existsSync("./.env")) require("dotenv").config({ path: "./.env" });
 
 const settings = {
-	token: process.env.API_TOKEN,
-	name: 'prhelperbot'
-}
+  token: process.env.API_TOKEN,
+  name: "prhelperbot"
+};
 
 export default class PRHelperBot {
-	constructor () {
-		this.prBot = new Bot(settings)
-	}
+  constructor() {
+    this.prBot = new Bot(settings);
+  }
 
-	start () {
-		this.prBot.postMessageToUser('mike', 'I\'m just getting started')
-	}
+  start() {
+    // this.prBot.postMessageToUser('mike', 'I\'m just getting started')
+  }
 
-	sendMessage ({ action, prData, prUrl, title, description, reviewers, user, aviUrl, head, base, isMerged }) {
-		if (action === 'opened' || action === 'reopened') {
-			this.prBot.postMessageToGroup('cinebody-platform', '', openedAttachment(prData))
-		} else if (action === 'closed' && isMerged) {
-			this.prBot.postMessageToGroup('cinebody-platform', '', closedAttachment(prData))
-		}
-	}
+  sendMessage(
+    {
+      action,
+      prData,
+      prUrl,
+      title,
+      description,
+      reviewers,
+      user,
+      aviUrl,
+      head,
+      base,
+      isMerged
+    }
+  ) {
+    if (action === "opened" || action === "reopened") {
+      this.prBot.postMessageToGroup(
+        "cinebody-platform",
+        "",
+        openedAttachment(prData)
+      );
+    } else if (action === "closed" && isMerged) {
+      this.prBot.postMessageToGroup(
+        "cinebody-platform",
+        "",
+        closedAttachment(prData)
+      );
+    }
+  }
 }
 
 const openedAttachment = prData => ({
-	'link_names': '1',
-	'as_user': 'true',
-	'attachments':
-	[
-		{
-			'fallback': 'A Pull Request was opened',
-			'color': '#00BDF2',
-			'pretext': '',
-			'author_name': `${getSlackName(prData.user.login)}`,
-			'author_link': `https://github.com/${prData.user.login}`,
-			'author_icon': prData.user.avatar_url,
-			'title': prData.title,
-			'title_link': prData.html_url,
-			'text': prData.body,
-			'fields': [
-				{
-					'title': 'Head',
-					'value': prData.head.ref,
-					'short': true
-				},
-				{
-					'title': 'Base',
-					'value': prData.base.ref,
-					'short': true
-				},
-				{
-					'title': 'Reviewers',
-					'value': getReviewers(prData.requested_reviewers),
-					'short': true
-				}
-			]
-		}
-	]
-})
+  link_names: "1",
+  as_user: "true",
+  attachments: [
+    {
+      fallback: "A Pull Request was opened",
+      color: "#00BDF2",
+      pretext: "",
+      author_name: `${getSlackName(prData.user.login)}`,
+      author_link: `https://github.com/${prData.user.login}`,
+      author_icon: prData.user.avatar_url,
+      title: prData.title,
+      title_link: prData.html_url,
+      text: prData.body,
+      fields: [
+        {
+          title: "Head",
+          value: prData.head.ref,
+          short: true
+        },
+        {
+          title: "Base",
+          value: prData.base.ref,
+          short: true
+        },
+        {
+          title: "Reviewers",
+          value: getReviewers(prData.requested_reviewers),
+          short: true
+        }
+      ]
+    }
+  ]
+});
 
 const closedAttachment = prData => ({
-	'link_names': '1',
-	'as_user': 'true',
-	'attachments':
-	[
-		{
-			'fallback': 'A pull request was merged by ',
-			'color': '#6F42C1',
-			'text': 'A pull request was merged by Mike'
-		}
-	]
-})
+  link_names: "1",
+  as_user: "true",
+  attachments: [
+    {
+      fallback: "A pull request was merged by ",
+      color: "#6F42C1",
+      text: "A pull request was merged!"
+    }
+  ]
+});
 
 const warnAttachment = prData => ({
-	'link_names': '1',
-	'as_user': 'true',
-	'attachments':
-	[
-		{
-			'fallback': 'A pull request is getting stale',
-			'color': '#E8412F',
-			'title': 'The following pull request has been open for over two hours. Please consider reviewing.'
-		}
-	]
-})
+  link_names: "1",
+  as_user: "true",
+  attachments: [
+    {
+      fallback: "A pull request is getting stale",
+      color: "#E8412F",
+      title: "The following pull request has been open for over two hours. Please consider reviewing."
+    }
+  ]
+});
--- src/server.js
+++ src/server.js
@@ -1,31 +1,30 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import PRHelperBot from './prhelperbot'
+import express from "express";
+import bodyParser from "body-parser";
+import PRHelperBot from "./prhelperbot";
 
-const app = express()
-const prBot = new PRHelperBot()
+const app = express();
+const prBot = new PRHelperBot();
 
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
+app.all("/", function(req, res) {
+  res.send("Hello World!");
+});
 
-app.all('/', function (req, res) {
-	res.send('Hello World!')
-})
+app.post("/payload", (req, res) => {
+  res.status(200).send("OK");
+  console.log("request", req.body.action);
+  const prData = req.body.pull_request;
+  const action = req.body.action;
+  if (action === "opened" || action === "reopened" || action === "closed") {
+    prBot.sendMessage({
+      action,
+      prData
+    });
+  }
+});
 
-app.post('/payload', (req, res) => {
-	res.status(200).send('OK')
-	console.log('request', req.body.action)
-	const prData = req.body.pull_request
-	const action = req.body.action
-	if (action === 'opened' || action === 'reopened' || action === 'closed') {
-		prBot.sendMessage({
-			action,
-			prData
-		})
-	}
-})
-
-app.listen(process.env.PORT || 4000, function () {
-	console.log('Listening for PRs!')
-	prBot.start()
-})
+app.listen(process.env.PORT || 4000, function() {
+  console.log("Listening for PRs!");
+  prBot.start();
+});
