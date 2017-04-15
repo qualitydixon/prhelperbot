207d727510c40787171cb4593daed7f5ee16aedb
Mike Dixon <qualitydixon@gmail.com>
2017-04-15 11:52:34 -0600

    Apply prettier format

--- src/prhelperbot.js
+++ src/prhelperbot.js
@@ -1,100 +1,95 @@
-import Bot from "slackbots";
-import fs from "fs";
-import { getSlackName, getReviewers } from "./helpers";
+import Bot from 'slackbots'
+import fs from 'fs'
+import { getSlackName, getReviewers } from './helpers'
 
-if (fs.existsSync("./.env")) require("dotenv").config({ path: "./.env" });
+if (fs.existsSync('./.env')) require('dotenv').config({ path: './.env' })
 
 const settings = {
-  token: process.env.API_TOKEN,
-  name: "prhelperbot"
-};
+	token: process.env.API_TOKEN,
+	name: 'prhelperbot',
+}
 
 export default class PRHelperBot {
-  constructor() {
-    this.prBot = new Bot(settings);
-  }
+	constructor() {
+		this.prBot = new Bot(settings)
+	}
 
-  start() {
-    // this.prBot.postMessageToUser('mike', 'I\'m just getting started')
-  }
+	start() {
+		// this.prBot.postMessageToUser('mike', 'I\'m just getting started')
+	}
 
-  sendMessage(
-    {
-      action,
-      prData
-    }
-  ) {
-    if (action === "opened" || action === "reopened") {
-      this.prBot.postMessageToGroup(
-        "cinebody-platform",
-        "",
-        openedAttachment(prData)
-      );
-    } else if (action === "closed" && prData.merged) {
-      this.prBot.postMessageToGroup(
-        "cinebody-platform",
-        "",
-        closedAttachment(prData)
-      );
-    }
-  }
+	sendMessage({ action, prData }) {
+		if (action === 'opened' || action === 'reopened') {
+			this.prBot.postMessageToGroup(
+				'cinebody-platform',
+				'',
+				openedAttachment(prData)
+			)
+		} else if (action === 'closed' && prData.merged) {
+			this.prBot.postMessageToGroup(
+				'cinebody-platform',
+				'',
+				closedAttachment(prData)
+			)
+		}
+	}
 }
 
 const openedAttachment = prData => {
-  const fields = prData.requested_reviewers.length > 0
-    ? [
-        {
-          title: "Reviewers",
-          value: getReviewers(prData.requested_reviewers),
-          short: true
-        }
-      ]
-    : [];
-  return {
-    link_names: "1",
-    as_user: "true",
-    attachments: [
-      {
-        fallback: `${getSlackName(prData.user.login)} opened a pull request`,
-        color: "#00BDF2",
-        pretext: "",
-        author_name: `${getSlackName(prData.user.login)}`,
-        author_link: `https://github.com/${prData.user.login}`,
-        author_icon: prData.user.avatar_url,
-        title: prData.title,
-        title_link: prData.html_url,
-        text: prData.body,
-        fields: fields
-      }
-    ]
-  };
-};
+	const fields = prData.requested_reviewers.length > 0
+		? [
+				{
+					title: 'Reviewers',
+					value: getReviewers(prData.requested_reviewers),
+					short: true,
+				},
+			]
+		: []
+	return {
+		link_names: '1',
+		as_user: 'true',
+		attachments: [
+			{
+				fallback: `${getSlackName(prData.user.login)} opened a pull request`,
+				color: '#00BDF2',
+				pretext: '',
+				author_name: `${getSlackName(prData.user.login)}`,
+				author_link: `https://github.com/${prData.user.login}`,
+				author_icon: prData.user.avatar_url,
+				title: prData.title,
+				title_link: prData.html_url,
+				text: prData.body,
+				fields: fields,
+			},
+		],
+	}
+}
 
 const closedAttachment = prData => {
-  const message = prData.merged_by !== null
-    ? `${getSlackName(prData.merged_by.login)} merged ${getSlackName(prData.user.login)}'s pull request.`
-    : "A pull request was merged.";
-  return {
-    link_names: "1",
-    as_user: "true",
-    attachments: [
-      {
-        fallback: message,
-        color: "#6F42C1",
-        text: `${message} You rock!`
-      }
-    ]
-  };
-};
+	const message = prData.merged_by !== null
+		? `${getSlackName(prData.merged_by.login)} merged ${getSlackName(prData.user.login)}'s pull request.`
+		: 'A pull request was merged.'
+	return {
+		link_names: '1',
+		as_user: 'true',
+		attachments: [
+			{
+				fallback: message,
+				color: '#6F42C1',
+				text: `${message} You rock!`,
+			},
+		],
+	}
+}
 
 const warnAttachment = prData => ({
-  link_names: "1",
-  as_user: "true",
-  attachments: [
-    {
-      fallback: "A pull request is getting stale",
-      color: "#E8412F",
-      title: "The following pull request has been open for over two hours. Please consider reviewing."
-    }
-  ]
-});
+	link_names: '1',
+	as_user: 'true',
+	attachments: [
+		{
+			fallback: 'A pull request is getting stale',
+			color: '#E8412F',
+			title: 'The following pull request has been open for over two hours. Please consider reviewing.',
+		},
+	],
+})
--- src/server.js
+++ src/server.js
@@ -1,32 +1,38 @@
-import express from "express";
-import bodyParser from "body-parser";
-import PRHelperBot from "./prhelperbot";
+import express from 'express'
+import bodyParser from 'body-parser'
+import PRHelperBot from './prhelperbot'
 
-const app = express();
-const prBot = new PRHelperBot();
+const app = express()
+const prBot = new PRHelperBot()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.all("/", function(req, res) {
-  res.send("Hello World!");
-});
+app.all('/', function(req, res) {
+	res.send('Hello World!')
+})
 
-app.post("/payload", (req, res) => {
-  res.status(200).send("OK");
-  console.log("request action:", req.body.action);
-  console.log("**********************");
-  console.log("request data", req.body.pull_request);
-  const prData = req.body.pull_request;
-  const action = req.body.action;
-  if (action === "opened" || action === "reopened" || action === "closed") {
-    prBot.sendMessage({
-      action,
-      prData
-    });
-  }
-});
+app.post('/payload', (req, res) => {
+	res.status(200).send('OK')
+	console.log('request action:', req.body.action)
+	console.log('**********************')
+	console.log('request data', req.body.pull_request)
+	const prData = req.body.pull_request
+	const action = req.body.action
+	if (action === 'opened' || action === 'reopened' || action === 'closed') {
+		prBot.sendMessage({
+			action,
+			prData,
+		})
+	}
+})
+
+app.post('/pr', (req, res) => {
+	res.status(200).send('OK')
+	console.log('request action:', req)
+	console.log('**********************')
+})
 
 app.listen(process.env.PORT || 4000, function() {
-  console.log("Listening");
-  prBot.start();
-});
+	console.log('Listening')
+	prBot.start()
+})
